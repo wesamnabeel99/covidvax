a0c6f89da81ba01738d5937d0fb1837e970fd5dd
Wesam Nabeel <wesamezio@gmail.com>
2021-08-23 16:48:51 +0300

    add animation

--- app/src/main/java/com/example/covidvax/ui/SearchFragment.kt
+++ app/src/main/java/com/example/covidvax/ui/SearchFragment.kt
@@ -1,9 +1,13 @@
 package com.example.covidvax.ui
 
+import android.content.Context
 import android.transition.TransitionManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.airbnb.lottie.LottieDrawable
+import com.example.covidvax.R
 import com.example.covidvax.data.VaccineData
 import com.example.covidvax.databinding.FragmentSearchBinding
 import com.example.covidvax.utils.DataManager
@@ -15,12 +19,13 @@ class SearchFragment: BaseFragment<FragmentSearchBinding>() {
 
     override fun addCallbacks() {
     binding?.searchButton!!.setOnClickListener {
-        DataManager.countryStatistics(binding?.searchBar?.text!!.toString().lowercase())?.let {
-            TransitionManager.beginDelayedTransition(binding?.root)
-            bindTheData(
-                it
-            )
+        val countryStatistics =DataManager.countryStatistics(binding?.searchBar?.text!!.toString().lowercase())
+        if (countryStatistics!=null) {
+            bindTheData(countryStatistics)
+        } else {
+            notFound()
         }
+
     }
     }
 
@@ -30,13 +35,32 @@ class SearchFragment: BaseFragment<FragmentSearchBinding>() {
             fullyVaccinated.text = DataManager.roundTheNumber(lastDay.twoDoseVaccinated!!)
             dailyVaccinated.text = DataManager.roundTheNumber(lastDay.dailyVaccinations!!)
             lastUpdateText.text = "last update on : ${lastDay.date}"
+            country.text = lastDay.country.capitalize()
             vaccinatedPerHundred.text = "${lastDay.vaccinatedPerHundred}%"
+            when {
+                lastDay.vaccinatedPerHundred!!<30.0 -> {
+                    animation.setAnimation(R.raw.belowthirty)
+                    animation.playAnimation()
+                }
+                lastDay.vaccinatedPerHundred>=30.0&&lastDay.vaccinatedPerHundred<70.0 -> {
+                    animation.setAnimation(R.raw.vaccinating)
+                    animation.playAnimation()
+                }
+                else -> {
+                    animation.setAnimation(R.raw.fullyvaccinated)
+                    animation.playAnimation()
+                }
+            }
         }
     }
     private fun notFound() {
-//        binding?.apply {
-//            totalVaccinated.text = "notFound"
-//            fullyVaccinated.text = "notFound"
-//            oneDoseVaccinated.text = "notFound"
-       }
-    }
+        binding?.apply {
+            totalVaccinated.text = "N/A"
+            fullyVaccinated.text = "N/A"
+            dailyVaccinated.text = "N/A"
+            lastUpdateText.text = "N/A"
+            vaccinatedPerHundred.text = ""
+            country.text="not found"
+            animation!!.setAnimation(R.raw.notfound)
+            animation.playAnimation()
+        }    }}
