06114b640912e70e188753af2c91eac0f18ca062
Wesam Nabeel <wesamezio@gmail.com>
2021-08-29 18:12:59 +0300

    fix small problem

--- app/src/main/java/com/example/covidvax/ui/DataFragment.kt
+++ app/src/main/java/com/example/covidvax/ui/DataFragment.kt
@@ -1,21 +1,10 @@
 package com.example.covidvax.ui
 
-import android.os.Bundle
-import android.os.ParcelFileDescriptor.open
-import android.system.Os.open
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.example.covidvax.databinding.FragmentDataBinding
-import com.example.covidvax.ui.BaseFragment
 import com.example.covidvax.ui.adapters.VaccineAdapter
-import com.example.covidvax.utils.DataManager
-import com.example.covidvax.utils.DataParser
-import java.io.BufferedReader
-import java.io.FileReader
-import java.io.InputStreamReader
-import java.nio.channels.AsynchronousFileChannel.open
-import java.nio.channels.FileChannel.open
+import com.example.covidvax.data.DataManager
 
 class DataFragment: BaseFragment<FragmentDataBinding>() {
     override val LOG_TAG: String = "DATA_FRAGMENT"
@@ -38,9 +27,6 @@ class DataFragment: BaseFragment<FragmentDataBinding>() {
 
 
 
-    override fun setup(){
-
-    }
     private fun getData(){
         val adapter = VaccineAdapter(DataManager.vaccineListData)
         binding?.dataRecyclerview?.adapter = adapter
